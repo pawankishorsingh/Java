68c0f8c928ce2c4c9c2a7a6c456ca0b437e360fe
pawankishorsingh1981 <55680224+pawankishorsingh1981@users.noreply.github.com>
2022-05-29 07:43:54 +0530

    Better OOPS Code
    Output will be:
    Peeked: -1
    Current Stack: 30 | 20 | 10 |  (RunningSize = 3)
    Popped: 30
    Current Stack: 20 | 10 |  (RunningSize = 2)
    Stack is full, can't add.
    Peeked: 30000
    IsEmpty? false
    IsFull? true

--- Stack/CustomImplementationUsingLinkedList.java
+++ Stack/CustomImplementationUsingLinkedList.java
@@ -1,77 +1,100 @@
-/*
-Output:
-Is Empty?: true
-30
-20
-10
-
-Is Empty?: false
-Peek: 30
-Popped: 30
-Peek: 20
- */
-
-class Node{
-    int data;
-    Node next;
+// A Dynamic Programming based Java program to find minimum
+// number operations to convert str1 to str2
+class Node {
+    private int data;
+    private Node nextNode = null;
     public Node(int data){
         this.data = data;
     }
+    public int getData(){
+        return this.data;
+    }
+    public Node getNextNode(){
+        return this.nextNode;
+    }
+    public void setData(int data){
+        this.data= data;
+    }
+    public void setNextNode(Node node){
+        this.nextNode = node;
+    }
 }
 
-class Stack{
+class Stack {
+    int maxSize;
+    int runningSize = 0;
     Node top;
+
+    public Stack(int maxSize){
+        this.maxSize = maxSize;
+        this.top = null;
+    }
+
     public void push(int data){
-        if (top ==null){
+        if (runningSize == maxSize){
+            System.out.println("Stack is full, can't add.");
+            return;
+        }
+        this.runningSize++;
+        if (top==null){
             top = new Node(data);
             return;
         }
+
         Node temp = new Node(data);
-        temp.next = top;
-        top = temp;
+        temp.setNextNode(this.top);
+        this.top = temp;
     }
 
     public int pop(){
-        if (top == null){
+        if (this.top == null){
             return -1;
         }
-        int result = top.data;
-        top = top.next;
+        int result = this.top.getData();
+        this.top = top.getNextNode();
+        this.runningSize--;
         return result;
-    }
-
-    public boolean isEmpty(){
-        return top ==null;
     }
 
     public int peek(){
-        int result = top==null? -1: top.data;
-        return result;
+        return this.top == null? -1: this.top.getData();
+    }
+
+    public boolean isEmpty(){
+        return this.top == null;
+    }
+
+    public boolean isFull(){
+        return this.maxSize == this.runningSize;
     }
 
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        Node temp = top;
-        while (temp !=null){
-            sb.append(temp.data+"\n");
-            temp=temp.next;
+        Node temp = this.top;
+        while (temp != null){
+            sb.append(temp.getData()+ " | ");
+            temp = temp.getNextNode();
         }
-        return sb.toString();
+        return sb + " (RunningSize = " + this.runningSize + ")";
     }
+
 }
 
-public class Demo {
-    public static void main(String[] args) {
-        Stack stack = new Stack();
-        System.out.println("Is Empty?: "+stack.isEmpty());
+class Solution {
+    public static void main(String args[]) {
+        Stack stack = new Stack(3);
+        System.out.println("Peeked: " + stack.peek());
         stack.push(10);
         stack.push(20);
         stack.push(30);
-        System.out.println(stack);
-        System.out.println("Is Empty?: "+stack.isEmpty());
-        System.out.println("Peek: "+ stack.peek());
+        System.out.println("Current Stack: " + stack);
         System.out.println("Popped: " + stack.pop());
-        System.out.println("Peek: "+ stack.peek());
+        System.out.println("Current Stack: " + stack);
+        stack.push(30000);
+        stack.push(99999);
+        System.out.println("Peeked: " + stack.peek());
+        System.out.println("IsEmpty? " + stack.isEmpty());
+        System.out.println("IsFull? " + stack.isFull());
     }
 }
